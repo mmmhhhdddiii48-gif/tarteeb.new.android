ce0b764aebc58e6dfed451039a755c42bd840adc
mmmhhhdddiii48-gif <mmmhhhdddiii48@gmail.com>
2026-04-17 15:34:04 +0300

    Add Capacitor config file and finalize Android scripts

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tarteeb.app',
+  appName: 'ترتيب',
+  webDir: '.',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
